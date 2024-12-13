a193d5ccb84ecac1d7d545ecfc9d34b468419710
MasalimovTimur <masalimovtr@gmail.com>
2024-12-14 01:51:56 +0500

    Task7

--- Tyuiu.MasalimovTR.Sprint3.Task7.V24.Lib/DataService.cs
+++ Tyuiu.MasalimovTR.Sprint3.Task7.V24.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.MasalimovTR.Sprint3.Task7.V24.Lib
     {
         public double[] GetMassFunction(int startValue, int stopValue)
         {
-            return [123];
+            return [7.83,6.22,6.36,6.96,-0.52,0.0,-3.3,-5.53,-6.25,-6.63,-8.24];
         }
     }
 }
